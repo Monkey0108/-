b969012a476e30f89bebb1c7ee9b37abd54808ac
Mr.陈 <2530622629@qq.com>
2019-01-09 18:42:10 +0800

    second commit

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var multer = require("multer");
+var mysql = require("mysql");
+
 var upload = multer({ dest: "uploads/" });
 
-var cpUpload = upload.fields([{name:"files"}]);
+var cpUpload = upload.fields([{ name: "files" }]);
+
+var pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "20190108"
+});
 
 var app = express();
 
@@ -14,9 +23,17 @@ app.use(express.static("./www"));
 
 app.use("/api", cpUpload, (req, res) => {
   //console.log(req)
-  console.log(req.body, req.files);
-  
-  res.send("OK");
+  //console.log(req.body, req.files);
+  let fileArray=[];
+  req.files.files.forEach(file => {
+    fileArray.push(`('${file.originalname}','${file.filename}','${Math.floor(new Date() / 1000)}')`);
+  });
+  query = `INSERT INTO upload_table (originalname,filename,dateTime) VALUES${fileArray.join(",")}`;
+  pool.query(query,(err,result)=>{
+    if(err) throw err;
+    console.log(result)
+    res.send("OK");
+  })
 });
 
 app.listen("8888");
